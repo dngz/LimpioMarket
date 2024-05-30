5dbb6d513d9b2d1464fdf98259bc56d427f84670
Daniel Galvez Galvez <dan.galvez@duocuc.cl>
2024-05-30 15:22:54 -0400

    models actualizado
    añadi la clase usuario y estableci el usuario como foreign key dentro de la orden de compra

--- LimpioMarket/webMarket/models.py
+++ LimpioMarket/webMarket/models.py
@@ -1,4 +1,44 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
+
+class MiAdministradorDeUsuarios(BaseUserManager):
+    def crear_usuario(self, nombre_usuario, contrasena=None):
+        if not nombre_usuario:
+            raise ValueError('Los usuarios deben tener un nombre de usuario')
+
+        usuario = self.model(nombre_usuario=nombre_usuario)
+        usuario.set_password(contrasena)
+        usuario.save(using=self._db)
+        return usuario
+
+    def crear_superusuario(self, nombre_usuario, contrasena):
+        usuario = self.crear_usuario(nombre_usuario, contrasena=contrasena)
+        usuario.es_administrador = True
+        usuario.save(using=self._db)
+        return usuario
+
+class Usuario(AbstractBaseUser):
+    nombre_usuario = models.CharField(max_length=30, unique=True)
+    esta_activo = models.BooleanField(default=True)
+    es_administrador = models.BooleanField(default=False)
+
+    objects = MiAdministradorDeUsuarios()
+
+    USERNAME_FIELD = 'nombre_usuario'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.nombre_usuario
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.es_administrador
 
 class Producto(models.Model):
     nombre = models.CharField(max_length=100)
@@ -12,6 +52,7 @@ class Producto(models.Model):
         return self.cantidad * self.precio
 
 class OrdenDeCompra(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)  # Nuevo campo de usuario
     productos = models.ManyToManyField(Producto, related_name='orden_compra')
     fecha = models.DateTimeField(auto_now_add=True)
     direccion = models.CharField(max_length=255, default='')
