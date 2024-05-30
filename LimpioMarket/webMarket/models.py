d97185438657f238cc947f6808639547dd12563f
nigagato <fa.bravo@duocuc.cl>
2024-05-29 23:57:42 -0400

    orden de compra añadido, branch, css y funcionalidad

--- LimpioMarket/webMarket/models.py
+++ LimpioMarket/webMarket/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    cantidad = models.IntegerField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.nombre
+
+class OrdenDeCompra(models.Model):
+    productos = models.ManyToManyField(Producto, related_name='orden_compra')
+    fecha = models.DateTimeField(auto_now_add=True)
+    direccion = models.CharField(max_length=255, default='')
+    correo = models.EmailField(default='')
+    rut = models.CharField(max_length=10, default='')
+
+    def total(self):
+        return sum(producto.precio * producto.cantidad for producto in self.productos.all())
+
+    def __str__(self):
+        return f"Orden de Compra #{self.id} - {self.rut}"
--- LimpioMarket/webMarket/views.py
+++ LimpioMarket/webMarket/views.py
@@ -1,4 +1,46 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Producto, OrdenDeCompra
+from django.db.models import F, Sum
+from django.http import JsonResponse
 
 def index(request):
     return render(request, 'index.html')  # Asumo que tienes un template llamado 'index.html'
+
+def orden_de_compra(request):
+    productos = Producto.objects.all()
+    total_precio = productos.aggregate(total=Sum(F('cantidad') * F('precio')))['total'] or 0
+
+    if request.method == 'POST':
+        if 'agregar_producto' in request.POST:
+            nombre = request.POST['nombre']
+            cantidad = int(request.POST['cantidad'])
+            precio = float(request.POST['precio'])
+            Producto.objects.create(nombre=nombre, cantidad=cantidad, precio=precio)
+            return redirect('orden_compra')
+
+    return render(request, 'orden_compra.html', {'productos': productos, 'total_precio': total_precio})
+
+def guardar_orden_de_compra(request):
+    if request.method == 'POST':
+        direccion = request.POST['direccion']
+        correo = request.POST['correo']
+        rut = request.POST['rut']
+        productos = Producto.objects.all()
+
+        # Validación de la información de contacto
+        if not direccion or not correo or not rut:
+            return JsonResponse({'error': 'Por favor, complete todos los campos de información de contacto.'})
+
+        # Creación de la orden de compra
+        orden_compra = OrdenDeCompra.objects.create(direccion=direccion, correo=correo, rut=rut)
+        for producto in productos:
+            orden_compra.productos.add(producto)
+        orden_compra.save()
+
+        # Eliminar los productos añadidos a la orden de compra
+        productos.delete()
+
+        return JsonResponse({'success': '¡La orden de compra se ha guardado correctamente!'})
+    
+    return JsonResponse({'error': 'Método no permitido.'})
