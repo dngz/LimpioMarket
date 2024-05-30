cae410bedbdfaa676e4e3cd44214235c06df2a4b
Daniel Galvez Galvez <dan.galvez@duocuc.cl>
2024-05-30 15:12:52 -0400

    lista.html añadida styles.css modificado
    tambien he cambiado el models, urls, views, admin.

--- LimpioMarket/webMarket/urls.py
+++ LimpioMarket/webMarket/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('orden_compra/', orden_de_compra, name='orden_de_compra'),
     path('guardar_orden_de_compra/', guardar_orden_de_compra, name='guardar_orden_de_compra'),
     path('login/',login,name='LOGIN'),
-
+    path('lista_productos/', lista_productos, name='lista_productos'),
 ]
