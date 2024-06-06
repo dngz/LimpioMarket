2c0d6ea242491154a01eedfd35e0e7b54dc75cdb
nigagato <fa.bravo@duocuc.cl>
2024-06-05 21:19:23 -0400

    lista html, styles css, orden compra html, urls modificado

--- LimpioMarket/webMarket/urls.py
+++ LimpioMarket/webMarket/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', login, name='LOGIN'),
     path('orden_compra/', orden_de_compra, name='orden_de_compra'),
     path('guardar_orden_de_compra/', guardar_orden_de_compra, name='guardar_orden_de_compra'),
     path('login/',login,name='LOGIN'),
